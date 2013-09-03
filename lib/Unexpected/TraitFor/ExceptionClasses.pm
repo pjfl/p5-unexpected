d2f60cf8f1e033c1d9896de65bab7a3bca6248aa
pjfl <github@roxsoft.co.uk>
2013-09-03 11:58:21 +0100

    0.12.1 ::TF::EC requires subclasses to call has_ex

--- lib/Unexpected/TraitFor/ExceptionClasses.pm
+++ lib/Unexpected/TraitFor/ExceptionClasses.pm
@@ -3,7 +3,7 @@
 package Unexpected::TraitFor::ExceptionClasses;
 
 use namespace::sweep;
-use version; our $VERSION = qv( sprintf '0.11.%d', q$Rev: 3 $ =~ /\d+/gmx );
+use version; our $VERSION = qv( sprintf '0.12.%d', q$Rev: 1 $ =~ /\d+/gmx );
 
 use Unexpected::Functions   qw( inflate_message );
 use Moo::Role;
@@ -99,7 +99,7 @@ Unexpected::TraitFor::ExceptionClasses - Define an exception class hierarchy
 
 =head1 Version
 
-This documents version v0.11.$Rev: 3 $
+This documents version v0.12.$Rev: 1 $
 of L<Unexpected::TraitFor::ExceptionClasses>
 
 =head1 Description
