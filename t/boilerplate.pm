e87059c73e3df3fc9a6f2cacd0025a60eab92828
pjfl <github@roxsoft.co.uk>
2015-08-26 23:36:44 +0100

    Releasing v0.39

--- t/boilerplate.pm
+++ t/boilerplate.pm
@@ -24,6 +24,8 @@ BEGIN {
          plan skip_all => 'Two colons in $Bin path';
       $host eq 'digitalis'  and
          plan skip_all => 'Broken smoker a54c1c84-6bf5-1014-b4f9-dcd54300afcd';
+      # TODO: Remove this when service is restored
+      plan skip_all => 'CPAN Testing down Aug 2015';
    }
 }
 
